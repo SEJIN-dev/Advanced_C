ca98554d74d097055f7b7492d6e3802ec3911aa5
sejin <sejinlim.dev@gmail.com>
2020-02-05 23:02:33 +0900

    feat:Add another output value
    removed name and putted three way to understand on pointer

--- Char_and_String/ragged_array.c
+++ Char_and_String/ragged_array.c
@@ -5,17 +5,18 @@ void main(void){
 	int i=0;
 	
 	char *fruits[] = {
-	"aaaaaaaaaaaaaaaapple",
-	"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbblueberry",
+	"apple",
+	"blueberry",
 	"orange",
 	"melon"
 	};
 	
-	char *name = "sejin";
+	printf(" fruits[0] : %s\n",fruits[0]);
+	printf(" *fruits[0] : %d\n",*fruits[0]);
+	printf(" &fruits[0] : %c\n",&fruits[0]);
+	/*
 	for(i;i<4;i++){
 		printf(" name: %s:%ld\n",fruits[i],sizeof(fruits[i]));
 	};
-	printf("name:%s\n",name);
-	printf("*name:%d\n",*name);
-	printf("&name[0]:%s\n",&name[0]);
+	*/
 }
