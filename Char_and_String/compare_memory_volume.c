40ce1999c162c355717737f07a45e15089936382
sejin <sejinlim.dev@gmail.com>
2020-02-06 00:13:27 +0900

    upload:compare_memory_volume.c
    test code for memory test

---
+++ Char_and_String/compare_memory_volume.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void){
+	float f =3.14592;
+	char c[] ="3.14592";
+	
+	printf("size of f = %ld\n",sizeof(f));
+	printf("size of c = %ld\n",sizeof(c));
+}
