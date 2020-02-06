299a8eb0e5749a790530c898d5ad115c6ca6d48c
sejin <sejinlim.dev@gmail.com>
2020-02-07 03:17:58 +0900

    upload:compare_float_char.c
    compare float type with char type. if I stored data in float, data size
    is fixed at 4byte. Even though it's bigger.

---
+++ Char_and_String/compare_float_char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(void){
+	float f= 3.14159134213111111111111111111111111111111111111111111111111112;
+	
+	printf("크기: %ld\n", sizeof(f));
+	printf("f=%f\n",f);
+	
+	
+}
