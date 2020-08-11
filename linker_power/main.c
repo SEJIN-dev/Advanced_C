29afa263a893cf869c3991322abdb5321b769962
SEJIN <sejin-dev@naver.com>
2020-08-11 04:10:05 -0400

    Feat: add extern option
    extern option is performing when excute main.c from power.c

--- linker_power/main.c
+++ linker_power/main.c
@@ -5,14 +5,18 @@ int main(int argc, char *argv[])
 {
 	int x,y;
 	int return_value=0;
+	extern int i; 
+	
+	
 	printf("Enter x: ");
 	scanf("%d",&x);
 	printf("Enter y: ");
 	scanf("%d",&y);
 	
-
+	printf("before performing program, extern i = %d\n",i); 
 	printf("%d to the power of %d is %d\n", x,y,power(x,y,&return_value));
 	printf("call by reference is :%d\n", return_value);
 	
+	printf("after performing program, extern i = %d\n",i); 
 	return 0;
 }
--- linker_power/power.c
+++ linker_power/power.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "power.h"
 
+int i;
+
 int power(int x, int y, int *return_value)
 {
-	int i;
 	int result=1;
 	
 	for(i =0; i<y; i++){
